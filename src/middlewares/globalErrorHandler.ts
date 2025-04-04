594b29d462a302f37f680d3604aa5fbdd3973fa5
Evan Kasky <ekasky25@gmail.com>
2025-04-04 18:24:00 -0400

    Modifies global error handler to respond to ApiError's

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { logger } from '../utils/logger';
+import { ApiError } from '../utils/ApiError';
 
 export const globalErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
@@ -7,10 +8,23 @@ export const globalErrorHandler = (err: Error, req: Request, res: Response, next
     // This info will not be sent to the client
     logger.error(err);
 
+    // Handle all ApiError responses here
+    if(err instanceof ApiError) {
+
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        });
+
+    } 
     // Send a uniform, clean, and vauge error response back to the client
-    res.status(500).json({
-        success: false,
-        message: 'Internal Server Error'
-    });
+    else {
+        
+        res.status(500).json({
+            success: false,
+            message: 'Internal Server Error'
+        });
+
+    }
 
 };
