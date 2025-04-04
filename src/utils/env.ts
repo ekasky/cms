e9ccc6972940823f0f3f8880075c778a877980b8
Evan Kasky <ekasky25@gmail.com>
2025-04-04 00:05:02 -0400

    Adds function to get env as a number

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -13,3 +13,20 @@ export const getEnv = (key: string, required = true): string => {
     }
 
 };
+
+export const getEnvAsNumber = (key: string, required = true): number => {
+
+    // Read the variable in as a string
+    const raw: string = getEnv(key, required);
+
+    // Convert the string to a number
+    const value: number = Number(raw);
+
+    // Ensure that the converted string is actually a real number
+    if(isNaN(value)) {
+        throw new Error(`Environment variable "${key}" must be a number. Got "${raw}"`);
+    }
+
+    return value;
+
+};
