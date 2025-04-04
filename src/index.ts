f1a01113ca9ccb4ce0f07cedb6ee0ed76c146619
Evan Kasky <ekasky25@gmail.com>
2025-04-04 00:42:39 -0400

    Starts a ExpressJS server on the defined port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
+import 'dotenv/config';
+import express from 'express';
+import { PORT } from './config/config';
 import { logger } from './utils/logger';
 
-logger.info('Hello World');
-logger.error('This is a error');
+const startServer = async (): Promise<void> => {
+
+    // Creates a new Express.js application instance.
+    const app = express();
+
+    // Middleware to parse incoming JSON requests.
+    // This allows the application to handle JSON data sent in the request body.
+    app.use(express.json());
+    
+    // Middleware to parse incoming URL-encoded requests.
+    // 'extended: true' allows parsing of rich objects and arrays encoded in the URL-encoded format.
+    // This is necessary for handling form submissions and other URL-encoded data.
+    app.use(express.urlencoded({ extended: true }));
+    
+    // Starts the Express.js server and listens for incoming requests on the specified PORT.
+    app.listen(PORT, () => {
+    
+        logger.info(`Server is running at http://localhost:${PORT}`);
+
+    });
+
+};
+
+startServer();
