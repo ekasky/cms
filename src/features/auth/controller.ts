661908b6d8bb4eed32e524d2db404b747b8772cc
Evan Kasky <ekasky25@gmail.com>
2025-04-04 19:01:15 -0400

    calls the register user service in the register controller

--- src/features/auth/controller.ts
+++ src/features/auth/controller.ts
@@ -1,12 +1,19 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { registerUser } from './services/auth';
 
 
-export const registerUserController = async (req: Request, res: Response) => {
+export const registerUserController = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
 
+        const result = await registerUser(req.body);
+
+        // If we got to this point the registration was successful
+        res.status(201).json(result);
+
     } catch(error) {
-        
+        // Passes errors to the global error handler
+        next(error);
     }
 
 };
